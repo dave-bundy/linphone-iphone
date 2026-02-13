fcf2ef5bd3c0909cfc9344c1ea8f174e1b7fadde
David Bundy <dbundy@startensystems.com>
2026-02-13 15:27:03 -0500

    working

--- Linphone/GeneratedGit.swift
+++ Linphone/GeneratedGit.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public enum AppGitInfo {
     public static let branch = "dev/dbundy-sdk-integration"
-    public static let commit = "69c42639f"
+    public static let commit = "2378bae19"
     public static let tag = "SDKbaseline"
 }
